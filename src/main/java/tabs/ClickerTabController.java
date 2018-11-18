61e313fae9f687385e76a3cfe82a147a16002a39
JoaquimAlfredoRibeiro <joaquimalfredoribeiro@gmail.com>
2018-11-18 18:41:13 +0000

    ClickerTab fxml first version

--- src/main/java/tabs/ClickerTabController.java
+++ src/main/java/tabs/ClickerTabController.java
@@ -1,19 +1,59 @@
 package tabs;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import org.apache.log4j.Logger;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXRadioButton;
+import javafx.scene.control.TextField;
 
 public class ClickerTabController {
+
 
     private final static Logger LOG = Logger.getLogger(ClickerTabController.class);
 
     @FXML
-    private Button test;
+    private TextField hoursTextField;
 
     @FXML
-    private void onTest() {
-        System.out.println("Clicker End Point");
-    }
+    private TextField minutesTextField;
+
+    @FXML
+    private TextField secondsTextField;
+
+    @FXML
+    private TextField millisecondsTextField;
+
+    @FXML
+    private JFXRadioButton repeatRadioButton;
+
+    @FXML
+    private TextField repeatTextField;
+
+    @FXML
+    private JFXRadioButton repeatUntilStoppedRadioButton;
+
+    @FXML
+    private JFXRadioButton currentLocationRadioButton;
+
+    @FXML
+    private JFXRadioButton pickLocationRadioButton;
+
+    @FXML
+    private TextField xTextField;
+
+    @FXML
+    private TextField yTextField;
+
+    @FXML
+    private JFXButton startButton;
+
+    @FXML
+    private JFXButton stopButton;
+
+    @FXML
+    private JFXButton setHotkeyButton;
+
+    @FXML
+    private JFXButton helpButton;
 
 }
